e18d0ba37f7eee8166603d14dbef175410f2edd0
bonesbro <bonesbro@users.noreply.github.com>
2023-04-01 21:30:16 -0700

    Working version

--- CraftableJute/CraftableJute.cs
+++ CraftableJute/CraftableJute.cs
@@ -1,4 +1,6 @@
-﻿using BepInEx;
+﻿using System;
+using BepInEx;
+using Jotunn.Configs;
 using Jotunn.Entities;
 using Jotunn.Managers;
 using Jotunn.Utils;
@@ -7,12 +9,11 @@ namespace bonesbro
 {
 	[BepInPlugin(PluginGUID, PluginName, PluginVersion)]
 	[BepInDependency(Jotunn.Main.ModGuid)]
-	//[NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
 	internal class CraftableJute : BaseUnityPlugin
 	{
 		public const string PluginGUID = "bonesbro.val.craftablejute";
 		public const string PluginName = "CraftableJute";
-		public const string PluginVersion = "0.0.1";
+		public const string PluginVersion = "1.0.0";
 		
 		// Use this class to add your own localization to the game
 		// https://valheim-modding.github.io/Jotunn/tutorials/localization.html
@@ -20,11 +21,34 @@ namespace bonesbro
 
 		private void Awake()
 		{
-			// Jotunn comes with its own Logger class to provide a consistent Log style for all mods using it
-			Jotunn.Logger.LogInfo("ModStub has landed");
-			
-			// To learn more about Jotunn's features, go to
-			// https://valheim-modding.github.io/Jotunn/tutorials/overview.html
+			AddRecipes();
+		}
+
+		private void AddRecipes()
+		{
+			Jotunn.Logger.LogInfo("Setting up recipes");
+
+			RecipeConfig redJute = new RecipeConfig
+			{
+				Item = "JuteRed",
+				CraftingStation = "piece_artisanstation",
+				Amount = 1
+			};
+			redJute.AddRequirement(new RequirementConfig("LinenThread", 3));
+			redJute.AddRequirement(new RequirementConfig("Raspberry", 1));
+			ItemManager.Instance.AddRecipe(new CustomRecipe(redJute));
+
+			RecipeConfig blueJute = new RecipeConfig
+			{
+				Item = "JuteBlue",
+				CraftingStation = "piece_artisanstation",
+				Amount = 1
+			};
+			blueJute.AddRequirement(new RequirementConfig("LinenThread", 3));
+			blueJute.AddRequirement(new RequirementConfig("MushroomMagecap", 1));
+			ItemManager.Instance.AddRecipe(new CustomRecipe(blueJute));
+
+			Jotunn.Logger.LogInfo("Finished adding recipes");
 		}
 	}
 }
--- CraftableJute/README.md
+++ CraftableJute/README.md
@@ -1,14 +1,14 @@
-﻿# JotunnModStub
-Upon building for release, this README.md is copied into the `Package` folder for thunderstore packaging. Remember to also edit the manifest.json and supply your own mod icon.
+﻿# Craftable Jute
+This simple mod allows you to craft red and blue jute at the artisan table.
 
-## Installation (manual)
+## Recipes
+3 Linen Thread + 1 Raspberry = 1 Red Jute
+3 Linen Thread + 1 Magecap = 1 Blue Jute
 
+Both recipes are crafted at the Artisan Table.
 
-## Features
-
+## Source Code
+https://github.com/bonesbro/CraftableJute
 
 ## Changelog
-
-
-## Known issues
-You can find the github at:
+1.0 - Initial release
